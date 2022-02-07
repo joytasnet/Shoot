94035766f361ce7583f48a8e799ebe24454708cb
mjpurin <mjpurin@gmail.com>
2022-02-07 09:00:54 +0900

    Shoot完成

---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Create());
+        
+    }
+    IEnumerator Create() {
+        float delta = 1.5f;
+        while (true) {
+            GameObject obj = Instantiate(
+                prefab,
+                new Vector3(
+                    Random.Range(-10f, 10f),
+                    Random.Range(8f, 12f),
+                    Random.Range(-2f, 3f)
+                    ),
+                Quaternion.identity
+            );
+            obj.GetComponent<CubeController>().SetGameManager(gm);
+            yield return new WaitForSeconds(delta);
+
+            if(delta > 0.5f) {
+                delta -= 0.05f;
+            }
+
+        }
+    }
+}
---
+++ Assets/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject prefab;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject obj = Instantiate(prefab);
+            obj.transform.parent = transform;
+            obj.transform.localPosition = Vector3.zero;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 dir = ray.direction.normalized;
+            //obj.GetComponent<Rigidbody>().velocity = dir * 100f;
+            obj.GetComponent<Rigidbody>().AddForce(dir * 100f,ForceMode.Impulse);
+        }
+        
+    }
+}
